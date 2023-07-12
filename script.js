50a70fbfeb1f3ae30ea1fa076d299981f68130e1
jmyslins <134307714+jmyslins@users.noreply.github.com>
2023-07-12 13:37:47 -0400

    Update script.js
    Shiny App list script

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-// Fetch the data table file
-fetch('data.csv') // Replace with your data file name
+// Read the CSV file
+fetch('AppURLs.csv') // Read App file
   .then(response => response.text())
   .then(data => {
-    // Process the data
-    const parsedData = processData(data); // Implement your own data processing logic
+    const lines = data.split('\n');
+    const appList = document.getElementById('appList');
 
-    // Create the scatter plot
-    const scatterPlot = document.getElementById('scatterPlot');
-    Plotly.newPlot(scatterPlot, parsedData, { type: 'scatter', mode: 'markers' });
+    // Skip the header row 
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i];
+      const cells = line.split(',');
+
+      const url = cells[0].trim();
+      const sentence = cells[1].trim();
+
+      // Create the hyperlink element from APP URLs with Sentence being decription
+      const link = document.createElement('a');
+      link.href = url;
+      link.textContent = sentence;
+
+      // Create the list item element and append the hyperlink
+      const listItem = document.createElement('li');
+      listItem.appendChild(link);
+
+      // Append the list item to the appList
+      appList.appendChild(listItem);
+    }
   })
   .catch(error => {
     console.error('Error loading data:', error);
   });
-
-function processData(data) {
-  // Implement your own data processing logic to parse the CSV or JSON data
-  // and return it in a suitable format for Plotly.js
-  // Example: Convert CSV to an array of objects
-  const rows = data.split('\n');
-  const headers = rows[0].split(',');
-  const parsedData = [];
-
-  for (let i = 1; i < rows.length; i++) {
-    const row = rows[i].split(',');
-    const dataPoint = {};
-    for (let j = 0; j < headers.length; j++) {
-      dataPoint[headers[j]] = row[j];
-    }
-    parsedData.push(dataPoint);
-  }
-
-  return parsedData;
-}
